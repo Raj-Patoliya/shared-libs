3d5ff5079efb1e3531681417da9be27cb9593d1f
Raj-Patoliya <rjpeace888@gmail.com>
2023-11-06 18:22:37 +0530

    WIP

--- vars/nodeScript.groovy
+++ vars/nodeScript.groovy
@@ -1,11 +1,4 @@
-// MySharedLibrary.groovy
+
 def call() {
     def fileFromLibrary = libraryResource("resources/index.js")
-   if (fileFromLibrary) {
-        sh "cat ${fileFromLibrary}" 
-        sh "ls"
-        sh "node index.js"
-    } else {
-        error("File 'index.js' not found in the shared library.")
-    }
 }
