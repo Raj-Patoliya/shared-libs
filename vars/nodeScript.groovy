2510c0abb641ea5e4e7356f6a899597116fb42b7
Raj-Patoliya <rjpeace888@gmail.com>
2023-11-06 18:48:00 +0530

    Done

--- vars/nodeScript.groovy
+++ vars/nodeScript.groovy
@@ -1,7 +1,5 @@
-// MySharedLibrary.groovy
 def call() {
     def fileResource = libraryResource("index.js")
     writeFile file: "index.js", text: fileResource
-    sh "ls"
     sh "node index.js"
 }
