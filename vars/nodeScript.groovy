f083e1e265e157729eb56672a8324000a9c94969
Raj-Patoliya <rjpeace888@gmail.com>
2023-11-06 18:40:12 +0530

    WIP

--- vars/nodeScript.groovy
+++ vars/nodeScript.groovy
@@ -1,11 +1,5 @@
 // MySharedLibrary.groovy
 def call() {
     def fileResource = libraryResource("index.js")
-   if (fileResource) {
-        def fileContents = readFile(fileResource)
-        return fileContents
-    } else {
-        error("File 'myFile.txt' not found in the shared library.")
-        return null
-    }
+    sh "cp ${fileResource} ."
 }
